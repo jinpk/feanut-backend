f7ff9aefdbab7f657949dbbb84b41edce48b908d
Jin Park <jinpk@users.noreply.github.com>
2023-02-24 20:08:28 +0900

    feat: Add user patch controller, dto

---
+++ src/users/dtos/update-user.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty, IsOptional, Length } from 'class-validator';
+import { Gender } from '../enums';
+
+export class PatchUserDto {
+  @ApiProperty({ title: '이름', required: false })
+  @IsOptional()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ title: 'Profile Image Id', required: false })
+  @IsOptional()
+  @IsNotEmpty()
+  profileImageId: string;
+
+  @ApiProperty({
+    title: '생일',
+    description: 'format: YYYYMMDD',
+    required: false,
+  })
+  @IsOptional()
+  @IsNotEmpty()
+  @Length(8, 8)
+  birth: string;
+
+  @ApiProperty({ title: '성별', enum: Gender, required: false })
+  @IsOptional()
+  @IsEnum(Gender)
+  gender: Gender;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,28 @@
-import { Body, ConflictException, Controller, Post } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Patch, Query, Req } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { PatchUserDto } from './dtos';
 import { UsersService } from './users.service';
 
 @ApiTags('User')
 @Controller('users')
+@ApiBearerAuth()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'PATCH User',
+    description: '수정하고싶은 field만 담아서 요청',
+  })
+  @ApiParam({ name: 'id', description: 'userId' })
+  async patchUser(
+    @Req() req,
+    @Query('id') id: string,
+    @Body() body: PatchUserDto,
+  ) {}
 }
