6cefe633140d96a7a68cdf70c695b5a49eb06c14
Jin Park <jinpk@users.noreply.github.com>
2023-02-24 14:37:30 +0900

    feat: Update user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,44 +1,49 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 import { Gender } from '../enums';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ collection: 'users' })
 export class User {
-  _id?: string;
+  // 로그인 이메일
+  // unique with isDeleted is false
+  @Prop({ lowercase: true })
+  email: string;
+
+  // 로그인 휴대폰번호
+  // unique with isDeleted is false
+  @Prop({})
+  phoneNumber: string;
+
   // 이름
   @Prop({ required: true })
   name: string;
 
-  // ID
-  // 삭제여부와 상관없이 유니크
-  @Prop({ required: true, unique: true })
-  username: string;
-
-  // 비밀번호
+  // 생일
+  // format: YYYYMMDD
   @Prop({ required: true })
-  password: string;
-
-  // 출생년도
-  @Prop({ required: true })
-  birthYear: number;
+  birth: string;
 
   // 성별
   @Prop({ required: true, enum: Gender })
   gender: string;
+
+  // 프로필 이미지 ID
+  @Prop({ type: Types.ObjectId })
+  profileImageId: Types.ObjectId;
 
   // 삭제여부
   @Prop({ default: false })
   isDeleted: boolean;
 
   // 삭제시간
-  @Prop()
-  deletedAt: Date;
+  @Prop({ default: null })
+  deletedAt?: Date;
 
   // 가입시간
   @Prop()
-  createdAt: Date;
+  createdAt?: Date;
 }
 
 export const UserSchmea = SchemaFactory.createForClass(User);
