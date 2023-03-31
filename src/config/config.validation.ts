f4234a6241cd0260dd8991f6690f8cdeea6c989c
Jin Park <jinpk@users.noreply.github.com>
2023-03-31 12:58:26 +0900

    refactor: Remove unused env

--- src/config/config.validation.ts
+++ src/config/config.validation.ts
@@ -8,13 +8,12 @@ export const validationSchema = joi.object({
   JWT_SECRET: joi.string().required(),
 
   FIREBASE_PROJECT_ID: joi.string().required(),
-  FIREBASE_PRIVATE_KEY_ID: joi.string().required(),
-  FIREBASE_PRIVATE_KEY: joi.string().required(),
-  FIREBASE_CLIENT_ID: joi.string().required(),
   FIREBASE_CLIENT_EMAIL: joi.string().required(),
+  FIREBASE_PRIVATE_KEY: joi.string().required(),
 
   GOOGLE_CLOUD_CLIENT_EMAIL: joi.string().required(),
   GOOGLE_CLOUD_PRIVATE_KEY: joi.string().required(),
+  
   GOOGLE_CLOUD_BUCKET: joi.string().required(),
 
   ALIGO_API_KEY: joi.string().required(),
