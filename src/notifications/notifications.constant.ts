38b8e95ec1a6f0220c67a1aedbf045dd2e21d286
Jin Park <jinpk@users.noreply.github.com>
2023-04-06 22:49:13 +0900

    feat: Update notification schema name

--- src/notifications/notifications.constant.ts
+++ src/notifications/notifications.constant.ts
@@ -1,2 +1,2 @@
 export const NOTIFICATION_MODULE_NAME = 'notifications';
-export const NOTIFICATION_USER_CONFIG_SCHEMA_NAME = 'usernotifications';
+export const NOTIFICATION_USER_CONFIG_SCHEMA_NAME = 'notification_user_configs';
