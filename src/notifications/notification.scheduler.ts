f891274a0f9589f624f31a6920952dae3f504f90
wonkee-choi <wonkic@gmail.com>
2023-04-09 01:52:42 +0900

    fix: cron time to UTC standard

--- src/notifications/notification.scheduler.ts
+++ src/notifications/notification.scheduler.ts
@@ -12,7 +12,7 @@ export class SchedulerService {
     private firebaseService: FirebaseService,
   ) {}
 
-  @Cron('0 30 12 * * 0-6')
+  @Cron('0 30 03 * * 0-6') //UTC시간. (한국시간 12시30분)
   async handleEveryLaunch() {
     this.logger.log('scheduling every launch time!');
     try {
@@ -34,7 +34,7 @@ export class SchedulerService {
     }
   }
 
-  @Cron('0 30 17 * * 0-6')
+  @Cron('0 30 08 * * 0-6') // UTC시간. (한국시간 17시30분)
   async handleEveryDinner() {
     this.logger.log('scheduling every launch time!');
     try {
@@ -56,26 +56,26 @@ export class SchedulerService {
     }
   }
 
-  // @Cron('0 30 12 * * 0-6')
-  @Cron('* * * * * *')
-  async handleEveryTest() {
-    this.logger.log('scheduling every test time!');
-    try {
-      const users = await this.notificationService.getListNotificationUsers();
-      if (!users.length) return;
+  // // @Cron('0 30 12 * * 0-6')
+  // @Cron('* * * * * *')
+  // async handleEveryTest() {
+  //   this.logger.log('scheduling every test time!');
+  //   try {
+  //     const users = await this.notificationService.getListNotificationUsers();
+  //     if (!users.length) return;
 
-      users.forEach((user) => {
-        this.firebaseService.sendPush({
-          tokens: [user.fcmToken],
-          title: user.contentText.split('\n').join(' '),
-          message: '참여할 수 있는 새로운 투표가 시작되었어요!',
-          payload: {},
-        });
-      });
-    } catch (error) {
-      this.logger.error(
-        `scheduling every test time to failed: ${JSON.stringify(error)}`,
-      );
-    }
-  }
+  //     users.forEach((user) => {
+  //       this.firebaseService.sendPush({
+  //         tokens: [user.fcmToken],
+  //         title: user.contentText.split('\n').join(' '),
+  //         message: '참여할 수 있는 새로운 투표가 시작되었어요!',
+  //         payload: {},
+  //       });
+  //     });
+  //   } catch (error) {
+  //     this.logger.error(
+  //       `scheduling every test time to failed: ${JSON.stringify(error)}`,
+  //     );
+  //   }
+  // }
 }
