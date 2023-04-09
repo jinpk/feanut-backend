4ac8fa8c6039b467b9565573aed400ca3a293829
Jin Park <jinpk@users.noreply.github.com>
2023-04-10 00:35:16 +0900

    feat: Update expires to 1h for access token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -86,7 +86,7 @@ export class AuthService {
       isAdmin: false,
     };
 
-    const accessToken = this.genToken(payload, '5s');
+    const accessToken = this.genToken(payload, '1h');
     const refreshToken = this.genToken({}, '30d');
 
     await this.usersService.updateRefreshTokenById(sub, refreshToken);
