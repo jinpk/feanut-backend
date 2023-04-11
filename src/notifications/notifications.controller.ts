b39d750642d3eb1152bb86b6f19dea70c3129730
wonkee-choi <wonkic@gmail.com>
2023-04-11 19:17:45 +0900

    fix: notification 수정

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -46,4 +46,10 @@ export class NotificationsController {
   async getUserNotificationSettings(@Param('userId') userId: string) {
     return await this.notificationsService.getNotificationUserConfig(userId);
   }
+
+  @Get('test')
+  @ApiOperation({ summary: 'test' })
+  async getUserNotificationgs() {
+    return await this.notificationsService.getListNotificationUsers();
+  }
 }
